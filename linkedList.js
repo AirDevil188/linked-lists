efff17967ddcd817c958c9e98853128328d2b195
AirDevil188 <90569889+AirDevil188@users.noreply.github.com>
2023-10-25 23:53:14 +0200

    Added prepend method to linkedList class
    Added prepend method to my linkedList module.

--- linkedList.js
+++ linkedList.js
@@ -3,7 +3,7 @@ import Node from "./node.js";
 export default class LinkedList {
   constructor() {
     this.head = null;
-    this.tail;
+    this.tail = null;
   }
 
   append(value) {
@@ -11,18 +11,21 @@ export default class LinkedList {
       const newNode = new Node(value);
       this.head = newNode;
       this.tail = newNode;
-      console.log(newNode);
     } else {
       const newNode = new Node(value);
       this.tail.nextNode = newNode;
       this.tail = newNode;
-      console.log(newNode);
+    }
+  }
+  prepend(value) {
+    if (this.head === null) {
+      const newNode = new Node(value);
+      this.head = newNode;
+      this.tail = newNode;
+    } else {
+      const newNode = new Node(value);
+      newNode.nextNode = this.head;
+      this.head = newNode;
     }
   }
 }
-
-/// [ 1 NODE (head) ] > [ 2  NODE ] > [ 3 NODE  (tail) > null]
-
-/// so if I have  [ 1 NODE (head) ] > [ 2  NODE ] > [ 3 NODE  (tail) > null]
-
-/// how can i keep the connection with [1 NODE] and [2 NODE]
--- main.js
+++ main.js
@@ -4,5 +4,4 @@ const list = new LinkedList();
 
 list.append(1);
 list.append(2);
-
-console.log(list);
+list.prepend(0);
