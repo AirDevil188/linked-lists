4af6b1185987ae3b0379d4aadbeb3c3e81517c19
AirDevil188 <90569889+AirDevil188@users.noreply.github.com>
2023-10-25 23:56:38 +0200

    added length variable for my size method

--- linkedList.js
+++ linkedList.js
@@ -4,6 +4,7 @@ export default class LinkedList {
   constructor() {
     this.head = null;
     this.tail = null;
+    this.lengthOfList = 0;
   }
 
   append(value) {
@@ -11,10 +12,12 @@ export default class LinkedList {
       const newNode = new Node(value);
       this.head = newNode;
       this.tail = newNode;
+      this.lengthOfList++;
     } else {
       const newNode = new Node(value);
       this.tail.nextNode = newNode;
       this.tail = newNode;
+      this.lengthOfList++;
     }
   }
   prepend(value) {
@@ -22,10 +25,12 @@ export default class LinkedList {
       const newNode = new Node(value);
       this.head = newNode;
       this.tail = newNode;
+      this.lengthOfList++;
     } else {
       const newNode = new Node(value);
       newNode.nextNode = this.head;
       this.head = newNode;
+      this.lengthOfList++;
     }
   }
 
